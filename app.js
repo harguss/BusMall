4c8967e4d49ce99544a3d0c4c3a9d8a64b0eb754
SHargus <87950738+harguss@users.noreply.github.com>
2022-01-26 20:51:52 -0600

    third image working

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const ProductPicture = function(name, imageSrc){
 };
 ProductPicture.allImages = [];
 
-// console.log(ProductPicture.allImages);
+console.log(ProductPicture.allImages);
 
 const renderNewProduct = function(firstIndex, secondIndex, thirdIndex){
   console.log(ProductPicture.allImages[firstIndex].url);
@@ -69,20 +69,6 @@ const pickNewProduct = function(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const handleClickOnProduct = function(event){
   console.log('clicking on the picture', event.target);
   if(totalClicks < 25){
@@ -90,7 +76,7 @@ const handleClickOnProduct = function(event){
     // console.log('why', thingWeClickedOn.id);
     const id = thingWeClickedOn.id;
 
-    if(id ==='first_product_image' || id === 'second_product_image'){
+    if(id ==='first_product_image' || id === 'second_product_image' || id === 'third_product_image'){
     //increment the image in the first slot
       if(id === 'first_product_image'){
         firstProductOnThePage.clicks++;
@@ -120,19 +106,55 @@ const handleClickOnProduct = function(event){
   }
 };
 
-
 productImageTagSection.addEventListener('click', handleClickOnProduct);
 
+////////////CHART
+{/* <canvas id="myChart" width="400" height="400"></canvas>
+<script>
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+</script> */}
 
 
 
-
-
-
+//////////////
 //create product obects
-new ProductPicture('boots', 'assets/boots.jpg');
-new ProductPicture('bag', 'assets/bag.jpg');
-new ProductPicture('banana', 'assets/banana.jpg');
+new ProductPicture('boots', 'assets/boots.jpg'); //0
+new ProductPicture('bag', 'assets/bag.jpg'); // 1
+new ProductPicture('banana', 'assets/banana.jpg'); //2
 new ProductPicture('bathroom', 'assets/bathroom.jpg');
 new ProductPicture('breakfast', 'assets/breakfast.jpg');
 new ProductPicture('bubblegum', 'assets/bubblegum.jpg');
@@ -144,7 +166,7 @@ new ProductPicture('pen', 'assets/pen.jpg');
 new ProductPicture('pet-sweep', 'assets/pet-sweep.jpg');
 new ProductPicture('scissors', 'assets/scissors.jpg');
 new ProductPicture('shark', 'assets/shark.jpg');
-new ProductPicture('sweep', 'assets/sweep.png');
+new ProductPicture('sweep', 'assets/sweep.jpg');
 new ProductPicture('tauntaun', 'assets/tauntaun.jpg');
 new ProductPicture('unicorn', 'assets/unicorn.jpg');
 new ProductPicture('water-can', 'assets/water-can.jpg');
